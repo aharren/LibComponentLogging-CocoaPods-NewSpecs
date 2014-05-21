2e8766bfc3dc1e12dd689dc18310da6e679eb5c8
Arne Harren <ah@0xc0.de>
2014-05-21 22:56:36 +0200

    re-format cocoapods and scenario lists

--- test/test.sh
+++ test/test.sh
@@ -3,13 +3,21 @@
 current_dir=`pwd`
 
 # for all supported CocoaPods versions
-cocoapods_versions=( "0.33.1" )
+cocoapods_versions=(
+  "0.33.1"
+)
 for cocoapods_version in ${cocoapods_versions[@]} ; do
 
   echo "- cocoapods ${cocoapods_version}"
 
   # for all scenarios
-  scenario_dirs=( "spec_lint" "plain_NSLog" "plain_LogFile" "plain_SystemLog" "plain_NSLogger" )
+  scenario_dirs=(
+    "spec_lint"
+    "plain_NSLog"
+    "plain_LogFile"
+    "plain_SystemLog"
+    "plain_NSLogger"
+  )
   for scenario_dir in ${scenario_dirs[@]} ; do
     echo "  - ${scenario_dir}"
     cd "${current_dir}/scenarios/${scenario_dir}"
