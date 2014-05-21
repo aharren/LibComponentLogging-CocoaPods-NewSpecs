74af003233967be79171a7974758dd4bd33ecc8c
Arne Harren <ah@0xc0.de>
2014-05-21 22:14:46 +0200

    add plain_SystemLog test scenario

--- test/test.sh
+++ test/test.sh
@@ -9,7 +9,7 @@ for cocoapods_version in ${cocoapods_versions[@]} ; do
   echo "- cocoapods ${cocoapods_version}"
 
   # for all scenarios
-  scenario_dirs=( "spec_lint" "plain_NSLog" "plain_LogFile" )
+  scenario_dirs=( "spec_lint" "plain_NSLog" "plain_LogFile" "plain_SystemLog" )
   for scenario_dir in ${scenario_dirs[@]} ; do
     echo "  - ${scenario_dir}"
     cd "${current_dir}/scenarios/${scenario_dir}"
