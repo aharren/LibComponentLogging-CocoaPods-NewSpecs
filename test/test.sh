94ebd3bd52868cb7289085024ca113a2c58f2799
Arne Harren <ah@0xc0.de>
2014-06-10 21:28:42 +0200

    add configure test scenario which uses Core, LogFile, qlog with 1.1 versions

--- test/test.sh
+++ test/test.sh
@@ -26,6 +26,7 @@ for cocoapods_version in ${cocoapods_versions[@]} ; do
     "plain_qlog"
     # lcl_configure pod variants
     "configure_Core_LogFile_qlog"
+    "configure_Core_LogFile_qlog_1_1"
     "configure_Core_SystemLog_qlog_UserDefaults"
     "configure_Core_LogFile_qlog_UserDefaults_RestKit_unembedded"
   )
